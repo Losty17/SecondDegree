678d3c98f89fc4f988434b519661dc95f2f06592
Vinícius <45098519+Losty17@users.noreply.github.com>
2019-11-03 13:52:20 -0300

    Update Equação de segundo grau.cpp

--- Equação de segundo grau.cpp
+++ Equação de segundo grau.cpp
@@ -17,33 +17,33 @@ int main(){
 			printf("\no--------------------o\n\n");
 		}else{
 			//delta
-		float delta = pow(b,2)-4*a*c;
-		if(delta == 0){
-			printf("\nDelta = 0\tRaiz do delta = 0\t");
-			float x = (-b + 0)/(2*a);
-			printf("x = %.2f\n", x);
-		}else if(delta < 0){
-			printf("\nDelta = %.2f\tRaiz do delta = Nenhuma raiz real\n", delta);
-		}else{
-			float raizdelta = sqrt(delta);
-			printf("\nDelta = %.2f\tRaiz do delta = %.2f\n", delta, raizdelta);
-			//bhaskara
-			float x1 = (-b+raizdelta)/(2*a);
-			float x2 = (-b-raizdelta)/(2*a);
-			printf("\nx1 = %.2f\tx2 = %.2f\n", x1, x2);
+			float delta = pow(b,2)-4*a*c;
+			if(delta == 0){
+				printf("\nDelta = 0\tRaiz do delta = 0\t");
+				float x = (-b + 0)/(2*a);
+				printf("x = %.2f\n", x);
+			}else if(delta < 0){
+				printf("\nDelta = %.2f\tRaiz do delta = Nenhuma raiz real\n", delta);
+			}else{
+				float raizdelta = sqrt(delta);
+				printf("\nDelta = %.2f\tRaiz do delta = %.2f\n", delta, raizdelta);
+				//bhaskara
+				float x1 = (-b+raizdelta)/(2*a);
+				float x2 = (-b-raizdelta)/(2*a);
+				printf("\nx1 = %.2f\tx2 = %.2f\n", x1, x2);
+			}
+			//direção da parabola
+			if(a > 0){
+				printf("\nAbertura da parabola virada para cima\n");
+			}else{	
+				printf("\nAbertura da parabola virada para baixo\n");
+			}
+			//vertices da parabola
+			float xvert = -b/(2*a);
+			float yvert = -delta/(4*a);
+			printf("\nX do vertice = %.2f\tY do vertice = %.2f\n", xvert, yvert);
+			printf("\no--------------------o\n\n");
+			}
+
 		}
-		//direção da parabola
-		if(a > 0){
-			printf("\nAbertura da parabola virada para cima\n");
-		}else{	
-			printf("\nAbertura da parabola virada para baixo\n");
-		}
-		//vertices da parabola
-		float xvert = -b/(2*a);
-		float yvert = -delta/(4*a);
-		printf("\nX do vertice = %.2f\tY do vertice = %.2f\n", xvert, yvert);
-		printf("\no--------------------o\n\n");
-		}
-		
-	}
 }
