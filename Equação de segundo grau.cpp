7c51f6a8e590feb9bc884da72ef3f397030c2599
Vinícius <45098519+Losty17@users.noreply.github.com>
2020-01-09 21:50:36 -0300

    Update Equação de segundo grau.cpp

--- Equação de segundo grau.cpp
+++ Equação de segundo grau.cpp
@@ -3,7 +3,8 @@
 
 int main(){
 	printf("@KKKBini©\n\n");
-	while(1 == 1){
+	while(True)
+	{
 		float a, b, c;
 		printf("Digite o valor de a: ");
 		scanf("%f", &a);
@@ -15,16 +16,19 @@ int main(){
 		if(a == 0){
 			printf("O valor de A e igual a 0, impossivel calcular.\n");
 			printf("\no--------------------o\n\n");
-		}else{
+		} else
+		{
 			//delta
 			float delta = pow(b,2)-4*a*c;
 			if(delta == 0){
 				printf("\nDelta = 0\tRaiz do delta = 0\t");
 				float x = (-b + 0)/(2*a);
 				printf("x = %.2f\n", x);
-			}else if(delta < 0){
+			} else if(delta < 0)
+			{
 				printf("\nDelta = %.2f\tRaiz do delta = Nenhuma raiz real\n", delta);
-			}else{
+			} else
+			{
 				float raizdelta = sqrt(delta);
 				printf("\nDelta = %.2f\tRaiz do delta = %.2f\n", delta, raizdelta);
 				//bhaskara
@@ -33,9 +37,11 @@ int main(){
 				printf("\nx1 = %.2f\tx2 = %.2f\n", x1, x2);
 			}
 			//direção da parabola
-			if(a > 0){
+			if(a > 0)
+			{
 				printf("\nAbertura da parabola virada para cima\n");
-			}else{	
+			} else
+			{	
 				printf("\nAbertura da parabola virada para baixo\n");
 			}
 			//vertices da parabola
@@ -43,7 +49,6 @@ int main(){
 			float yvert = -delta/(4*a);
 			printf("\nX do vertice = %.2f\tY do vertice = %.2f\n", xvert, yvert);
 			printf("\no--------------------o\n\n");
-			}
-
 		}
+	}
 }
